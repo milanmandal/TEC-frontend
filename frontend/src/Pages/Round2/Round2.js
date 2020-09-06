3da54e38559fd23ff8ed7ad359757649912a6539
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-09-06 21:18:10 +0530

    done

---
+++ frontend/src/Pages/Round2/Round2.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import CompanyCard from '../../Components/CompanyCard/CompanyCard'
+import Modal from 'react-modal';
+
+import './Round2.Styles.css'
+import Axios from 'axios';
+import Graph from '../../Components/Graph/Graph';
+import { connect } from 'react-redux';
+import { updateInvestedCompanies } from '../../Redux/User/UserActions';
+
+class StockMarket extends Component {
+    constructor(){
+        super()
+        this.state = {
+            capital: 0,
+            companies: null,
+            selectedCompany: null,
+            isPopUpOpen: false,
+            investedAmount: 0,
+            investedCompanies: []
+        }
+    }
+
+    componentDidMount(){
+        Axios.get('http://localhost:5000/stock/companylist',
+        {
+          headers:{
+            "authorization":"Bearer "+sessionStorage.usertoken
+          }
+        })
+        .then(res => this.setState({companies: res.data}))
+
+        this.setState({capital: this.props.currentUser.currentUser.score})
+    }
+
+    onclick = (e) => {
+        e.stopPropagation()
+        this.setState({
+            selectedCompany: this.state.companies[e.currentTarget.id],
+            isPopUpOpen: true
+        })
+    }
+
+    onInvestAmtChanged = (e) => {
+        e.preventDefault()
+        if(e.target.value*this.state.selectedCompany.data[this.state.selectedCompany.data.length-1].price <= this.state.capital){
+            this.setState({investedAmount: e.target.value*this.state.selectedCompany.data[this.state.selectedCompany.data.length-1].price})
+        }else{
+            alert('Investment Cannot be greater than Capital')
+            e.target.value=0
+        }
+    }
+
+    onInvest = (e) => {
+        e.preventDefault()
+        const {selectedCompany, investedAmount, capital, investedCompanies} = this.state
+        const numberOfShares = investedAmount/selectedCompany.data[this.state.selectedCompany.data.length-1].price
+        this.setState({isPopUpOpen: false})
+        if(investedAmount > 0){
+            if(numberOfShares >= selectedCompany.min){
+                if(investedCompanies.length < 2){
+                    this.setState({capital: capital-investedAmount})
+                    const existingCompany = investedCompanies.find((company) => company.name === selectedCompany.name)
+                    if(existingCompany){
+                        existingCompany.investedAmount = existingCompany.investedAmount + investedAmount
+                        existingCompany.returns = (selectedCompany.profitpercent+100)*existingCompany.investedAmount/100
+                    }else{
+                        investedCompanies.push({name: selectedCompany.name, investedAmount: investedAmount, returns: (selectedCompany.profitpercent+100)*investedAmount/100})
+                    }
+                    this.props.updateInvestedCompanies(investedCompanies)
+                }else{
+                    alert('Cannot invest in more than 2 stocks')
+                }
+            }else{
+                alert('Minimum no of shares are not purchased')
+            }
+        }else{
+            alert('Enter Valid Amount')
+        }
+    }
+
+    score = () => {
+        const {investedCompanies} = this.state
+        return(investedCompanies.reduce((a,b) => a + b.returns, 0).toFixed(2))
+    }
+
+    render() {
+        console.log(this.score())
+        return (
+            this.state.companies ?
+            <div className='round2-page'>
+                <h1 className='page-heading'>STOCK MARKET</h1>
+                <div className='stockmarket'>
+                    <div className='company-cards-container'>
+                    {
+                        this.state.companies.map( (company,index) => <CompanyCard index={index} company={company} onclick={this.onclick} key={company.name} />)
+                    }
+                    </div>
+                    <div className='my-portfolio'>
+                        <h1 className='heading'>My Portfolio</h1>
+                        <h3 className='capital'>Capital: {this.state.capital.toFixed(2)}</h3>
+                        <div className='investment-details'>
+                        {
+                            this.state.investedCompanies.map((investedCompany, index) => 
+                                <p key={index}>You have invested <strong>Rs. {investedCompany.investedAmount.toFixed(2)}</strong> in <strong>{investedCompany.name}</strong></p>
+                            )
+                        }
+                        </div>
+                    </div>
+                    <Link to='/round2/score'><button>Score</button></Link>
+                    <Modal isOpen={this.state.isPopUpOpen} onRequestClose={() => this.setState({isPopUpOpen: false})}>
+                    {
+                        this.state.selectedCompany ? 
+                        <div className='popup'>
+                            <div className='popup-details'>
+                                <h1 className='heading-primary'>{this.state.selectedCompany.name.toUpperCase()}</h1>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Price:</td>
+                                            <td>Rs. {this.state.selectedCompany.data[this.state.selectedCompany.data.length-1].price}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Last Updated:</td>
+                                            <td>{this.state.selectedCompany.data[this.state.selectedCompany.data.length-1].date}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <p>Minimum <strong>{this.state.selectedCompany.min}</strong> shares should be purchased</p>
+                                <form onSubmit={this.onInvest}>
+                                    <input
+                                        type='number'
+                                        placeholder='No. of Shares'
+                                        onChange={this.onInvestAmtChanged}
+                                    />
+                                    <button type='submit'>Invest</button>
+                                </form>
+                            </div>
+                            <div className='popup-contents'>
+                                <h1 className='heading-secondary'>Graph</h1>
+                                <Graph stockData={this.state.selectedCompany.data} />
+                                <h1 className='heading-secondary'>News</h1>
+                                <p className='news'>{this.state.selectedCompany.newsone}</p>
+                                <p className='news'>{this.state.selectedCompany.newstwo}</p>
+                                <p className='news'>{this.state.selectedCompany.newsthree}</p>
+                            </div>
+                        </div>
+                        : <div className='loading'>Loading...</div>
+                    }
+                    </Modal>
+                </div>
+            </div>
+            : <div className='loading'>Loading...</div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    currentUser: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+    updateInvestedCompanies: investedCompanies => dispatch(updateInvestedCompanies(investedCompanies))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StockMarket);
---
+++ frontend/src/Pages/Round2Rules.js/Round2Rules.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../../Components/Header/Header';
+import { setCurrentUser } from '../../Redux/User/UserActions';
+import { connect } from 'react-redux';
+import Axios from 'axios';
+
+class Round2Rules extends Component {
+
+    componentDidMount(){
+        Axios.get('http://localhost:5000/user/'+this.props.match.params.id,
+        {
+          headers:{
+            "authorization":"Bearer "+sessionStorage.usertoken
+          }
+        })
+        .then(response => {
+          if (response.status === 200) {
+            this.props.setCurrentUser(response.data)
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+
+    render(){
+        if(sessionStorage.usertoken){
+            return(
+                <div className='comprehension-rules-container'>
+                    <div className='comprehesion-rules'>
+                        <Header heading='Round-2 Rules' />
+                        <div className='rules'>
+                            <ol>
+                                <li>You will be given portfolios of 8 leading companies and you need to invest your capital in <strong>at most two</strong> companies (make your choice properly).</li>
+                                <li>Don’t Be Rushed into Making A Decision Out Of A Fear Of Missing Out.</li>
+                                <li>The company portfolio includes a news clipping and a graph of the stock pertaining to the company as shown below. The news clipping serves to give you an idea of how the graph might proceed in the future.</li>
+                                <li>The capital that you can invest is calculated from the number of correct answers provided in round 1. </li>
+                                <li>You can invest your capital in any combination but you are not allowed to invest more money than you earned in the previous round</li>
+                                <li>There will be a minimum no. of stocks for each company in which a participant can invest. The number of minimum stocks will be different for each company. 
+                                For example, Min shares for Reliance is 4. Once you will fulfil the above criterion, you’ll be eligible to buy any (greater than 4) no. of stocks for Reliance.
+                                </li>
+                                <li>Say for example you have invested in a stock which is listed at ₹100 and the graph gains 2% subsequently then you will receive a return of ₹102 and make a gain of ₹2.</li>
+                                <li>If instead of gaining, the graph falls by 2% subsequently, you will lose ₹2 from your initial investment and be returned ₹98.</li>
+                                <li>Time limit for selecting a company and investing in it is set to 45 minutes. This means that you must commit your capital to a company (or at max 2 companies) within forty five minutes.</li>
+                                <li>There is no elimination in this round.</li>
+                            </ol>
+                        </div>
+                        <div className='button'>
+                            <Link to='/stockmarket'><button>Stock Market</button></Link>
+                        </div>
+                    </div>
+                </div>
+            )
+        }else{
+            window.location='/';
+        }
+    }
+}
+
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Round2Rules);
